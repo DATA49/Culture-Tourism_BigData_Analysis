38400a8fde2a6abde5e5b2d9fceb7ef7c75b81f8
Lee ChaeHyeon <chlee1001@naver.com>
2021-06-28 00:44:09 +0900

    Update insta_crawling_file_v1.1

--- insta_crawling/test.py
+++ insta_crawling/test.py
@@ -28,61 +28,118 @@ driver.get(f'https://www.instagram.com/explore/tags/{hash_tag}/')
 sleep(10)
 
 # 데이터를 저장할 Dictionary
-insta_dict = {'id': [],
-              'date': [],
-              'hashtag': []}
+insta_dict = {'id': [],  # UserId
+              'date': [],  # 날짜
+              'maintext': [],  # 본문
+              'hashtag': []}  # 해시태그
 
 # 첫 번째 게시물 클릭
 first_post = driver.find_element_by_class_name('eLAPa')
 first_post.click()
 
-seq = 0
-eon = 10  # 크롤링할 게시물 개수
+count_extract = 0
+wish_num = 10  # 크롤링할 게시물 개수
 start = time.time()
+
+insta_obj_next_btn = 'a._65Bje.coreSpriteRightPaginationArrow'  # 다음 사진 버튼
+insta_obj_user_id = 'h2._6lAjh'  # 유저 아이디
+insta_obj_date = 'time.FH9sR.Nzb55'  # 날짜
+insta_obj_main_text = 'div.C4VMK'  # 본문
+insta_obj_main_tag = 'a.xil3i'  # 본문 해시태그
+# insta_obj_comment_id = 'h2_6lAjh'  # 댓글 아이디
+# insta_obj_comment_text = 'a.xil3i'
+
 while True:
     try:
-        if driver.find_element_by_css_selector('a._65Bje.coreSpriteRightPaginationArrow'):
-            if seq % 20 == 0:
-                print('{}번째 수집 중'.format(seq), time.time() - start, sep='\t')
+        if driver.find_element_by_css_selector(insta_obj_next_btn):
+            # if count_extract % 20 == 0:
+            print('{}번째 수집 중'.format(count_extract), time.time() - start, sep='\t')
 
             # id 정보 수집
             try:
-                info_id = driver.find_element_by_css_selector('h2._6lAjh').text
-                insta_dict['id'].append(info_id)
+                user_id = driver.find_element_by_css_selector(insta_obj_user_id).text
+                insta_dict['id'].append(user_id)
             except:
-                info_id = driver.find_element_by_css_selector('div.C4VMK').text.split()[0]
-                insta_dict['id'].append(info_id)
+                user_id = driver.find_element_by_css_selector('div.C4VMK').text.split()[0]
+                insta_dict['id'].append(user_id)
 
-            # 시간정보 수집
-            time_raw = driver.find_element_by_css_selector('time.FH9sR.Nzb55')
-            time_info = pd.to_datetime(time_raw.get_attribute('datetime')).normalize()
-            insta_dict['date'].append(time_info)
+            # 날짜정보 수집
+            try:
+                date_object = driver.find_element_by_css_selector(insta_obj_date)
+                date = date_object.get_attribute("title")
+                insta_dict['date'].append(date)
+            except:
+                date = None
+                insta_dict['date'].append(date)
+
+            # 본문
+            try:
+                raw_info = driver.find_element_by_css_selector(insta_obj_main_text).text.split()
+                text = []
+                for i in range(len(raw_info)):
+                    # 첫번째 text는 아이디니까 제외
+                    if i == 0:
+                        pass
+                    # 두번째부터 시작
+                    else:
+                        if '#' in raw_info[i]:
+                            pass
+                        else:
+                            text.append(raw_info[i])
+                clean_text = ' '.join(text)
+                insta_dict['maintext'].append(clean_text)
+            except:
+                main_text = None
+                insta_dict['maintext'].append(main_text)
 
             # hashtag 수집
-            raw_tags = driver.find_elements_by_css_selector('a.xil3i')
-            hash_tag = []
-            for i in range(len(raw_tags)):
-                if raw_tags[i].text == '':
-                    pass
-                else:
-                    hash_tag.append(raw_tags[i].text)
+            try:
+                main_tags = driver.find_elements_by_css_selector(insta_obj_main_tag)
+                hash_tag = []
+                for i in range(len(main_tags)):
+                    if main_tags[i].text == '':
+                        pass
+                    else:
+                        # print(main_tags[i].text.replace("#", ""))
+                        hash_tag.append(main_tags[i].text.replace("#", ""))
 
-            insta_dict['hashtag'].append(hash_tag)
+                insta_dict['hashtag'].append(hash_tag)
+            except:
+                hash_tag = None
+                insta_dict['hashtag'].append(hash_tag)
+            # # 댓글
+            # ## 더보기 버튼 클릭
+            # try:
+            #     while True:
+            #         try:
+            #             more_btn = driver.find_element_by_css_selector(comment_more_btn)
+            #             more_btn.click()
+            #         except:
+            #             break
+            # except:
+            #     print("----------------------fail to click more btn----------------------------")
+            #     continue
+            #
+            # ## 댓글 데이터
+            # try:
+            #     comment_data = {}
+            #     comment_ids_objects = driver.find_elements_by_css_selector(comment_ids_objects_css)
+            #     comment_texts_objects = driver.find_elements_by_css_selector(comment_texts_objects_css)
 
-            seq += 1
-
-            if seq == eon:
+            count_extract += 1
+            if count_extract == wish_num:
                 break
 
-            driver.find_element_by_css_selector('a._65Bje.coreSpriteRightPaginationArrow').click()
+            driver.find_element_by_css_selector(insta_obj_next_btn).click()
             sleep(1.5)
 
         else:
             break
 
     except:
-        driver.find_element_by_css_selector('a._65Bje.coreSpriteRightPaginationArrow').click()
+        driver.find_element_by_css_selector(insta_obj_next_btn).click()
         sleep(2)
 
 test = pd.DataFrame.from_dict(insta_dict)
-print(test)
+print(test.head)
+test.to_csv('insta.csv', encoding='utf-8-sig')
